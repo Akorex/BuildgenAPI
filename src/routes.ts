c96a862766f99054407fc27e0f4e9c13b795497a
Akorede Adewole <43486426+Akorex@users.noreply.github.com>
2025-05-05 12:30:12 +0100

    changed

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,7 @@
-import { GenRoute } from "./genidea/gen.route";
-
 import { Route } from "./routes.interface";
 import authRouter from "./authentication/auth.route";
 
-export const routes: Route[] = [new GenRoute()
+export const routes: Route[] = [
   {
     path: "auth",
     router: authRouter,
